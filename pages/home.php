f44a9bc2ac92f215e1e7581651061b01dedcfd52
jbgamingnl <jake@jbgaming.nl>
2025-04-05 03:05:34 +0200

    did shit

---
+++ pages/home.php
@@ -0,0 +1,24 @@
+<?php require_once("../php/head.php") ?>
+
+<head>
+    <title>|Home|</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <?php require_once("../php/header.php") ?>
+        <section id="front">
+            <h2>WELCOME TO MY SITE HAVE A LOOK AROUND!</h2>
+            <div class="hometextmain">
+                <div class="hometext1">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae perspiciatis optio enim adipisci omnis qui minus consequatur et, expedita nam impedit ipsa cum quisquam, ducimus, inventore sequi repudiandae magnam dolores?</p>
+                </div>
+                <div class="hometext2">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas, beatae ratione, recusandae libero exercitationem, aliquid nostrum nulla suscipit praesentium a sapiente voluptates dolor iusto! Iusto maiores magnam eaque qui quis!</p>
+                </div>
+            </div>
+            <div class="hometext1"></div>
+        </section>
+        <?php require_once("../php/header.php") ?>
+    </div>
+</body>
---
+++ php/header.php
@@ -0,0 +1,9 @@
+<header>
+<nav>
+    <img src="#" alt="logo">
+    <a href="#" style="color: gray;">home</a>
+    <a href="#" style="color: gray;">music</a>
+    <a href="#" style="color: gray;">socials</a>
+</nav>
+<h1>JB'S PORTFOLIO</h1>
+</header>
